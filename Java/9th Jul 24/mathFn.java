3a5f17368a0a3481ac366218597b9110f2dd8773
Shrishesha <shrisheshanarmatesshvara@gmail.com>
2024-07-17 10:02:32 +0530

    09th Jul

--- Java/9th Jul 24/mathFn.java
+++ Java/9th Jul 24/mathFn.java
@@ -3,7 +3,7 @@ public class mathFn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println("The Square root of " + a + "is ");
+        System.out.println("Square root of " + a + "is ");
         System.out.println(Math.sqrt(a));
         int x = sc.nextInt();
         System.out.println(a + " power " + x + " is ");
