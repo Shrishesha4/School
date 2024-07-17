1cc0159b71819d6cefab98fb3a9d5358cb50a74a
Shrishesha <shrisheshanarmatesshvara@gmail.com>
2024-07-17 13:36:18 +0530

    9th Jul

--- Java/9th Jul 24/add.java
+++
@@ -1,8 +0,0 @@
-public class add {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 9;
-        int c = a + b; 
-        System.out.println("Sum is: " + c);
-    }
-}
--- Java/9th Jul 24/conv.java
+++
@@ -1,8 +0,0 @@
-public class conv {
-    public static void main(String[] args) {
-        double c, f;
-        c = 32.0;
-        f = (9.0 / 5.0) * c + 32.0;
-        System.out.println("Celsius: " + c + " Fahrenheit: " + f);
-    }
-}
